10b7fad8dc956cac6d7b202cd9dae1b65013578b
Vincent Bernat <vincent@bernat.ch>
2025-06-14 08:16:30 +0200

    feat: provide a flake.nix to build with Nix
    One can use `nix build .`.
    
    On NixOS, there is only `/bin/sh` and `/usr/bin/env` outside the `/nix`
    directory, so some invocations of bash and env were updated for that.
    
    This would help for #100.

--- crates/tests/resources/bad_plugin.sh
+++ crates/tests/resources/bad_plugin.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 echo "Something went wrong" >&2
 echo "But in a good way" >&2
---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # Read version from Cargo.toml
+        cargoToml = builtins.fromTOML (builtins.readFile ./crates/tattoy/Cargo.toml);
+        version = cargoToml.package.version;
+
+        nativeBuildInputs = with pkgs; [
+          rustc
+          cargo
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          dbus
+          xorg.libxcb
+        ];
+        checkInputs = with pkgs; [
+          bash
+          nano
+          procps
+          cargo-nextest
+          mesa.llvmpipeHook
+        ];
+
+        tattoy = pkgs.rustPlatform.buildRustPackage {
+          pname = "tattoy";
+          inherit version;
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+
+          useNextest = true;
+          preCheck = "cargo build --all";
+
+          inherit nativeBuildInputs buildInputs checkInputs;
+
+          meta = with pkgs.lib; {
+            description = "Text-based compositor for modern terminals";
+            homepage = "https://tattoy.sh";
+            license = licenses.mit;
+            maintainers = with maintainers; [ vincentbernat ];
+            platforms = platforms.linux;
+          };
+        };
+      in
+      {
+        packages = {
+          default = tattoy;
+          tattoy = tattoy;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs checkInputs;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${tattoy}/bin/tattoy";
+        };
+      });
+}
